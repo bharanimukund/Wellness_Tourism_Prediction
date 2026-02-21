6b298a6591a7f12545404d8eb24026b3cd70b38d
bharanimukund <bharanimukund@gmail.com>
2026-02-21 14:11:38 +0000

    Added mlflow dependencies

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
-    folder_path="/content/tourism_project/deployment",     # the local folder containing your files
+    folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="bkrishnamukund/Wellness-Tourism-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                        # optional: subfolder path inside the repo
